203e6afd7acb4d2c6ccbdb1977070e7e8d9d8bbf
Andy Humes <andrew.humes@du.edu>
2022-08-23 16:48:36 -0600

    screenshot

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 80;
 
 const sess = {
-  secret: 'secret',
+  secret: process.env.SESSION_SECRET,
   cookie: { maxAge: 600000 },
   resave: false,
   saveUninitialized: true,
